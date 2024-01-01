13b7b3684efde56a9201957ed7c857e41a2fafe1
benkim077 <benkim0077@gmail.com>
2024-01-01 15:57:29 +0900

    Write HTML Template for about page

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('index', { title: 'Ben의 기술 블로그', description: '반갑습니다. 웹 프론트엔드 개발자 Ben입니다.', message: 'Hello there!' });
 })
 
 
