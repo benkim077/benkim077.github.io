406d8deca8c6466b6b0ef1ab8864082bbfd6191d
benkim077 <benkim0077@gmail.com>
2024-01-01 14:56:54 +0900

    Set Template Engine Pug
    - install pug
    - testing render templateHTML and response the HTML

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,13 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
