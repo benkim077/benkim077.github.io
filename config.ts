0c45e3f5957b2d2eda8a7e39c6d949f35d8130e8
benkim077 <benkim0077@gmail.com>
2024-01-04 14:38:44 +0900

    Generate Static HTML Files from .md
    - Implmement buildMarkdownFiles()
    - convert md to html and save at /docs/posts/filename/index.html

--- config.ts
+++ config.ts
@@ -3,6 +3,8 @@ export default {
     SRC: "src",
     VIEWS: "views",
     DOCS: "docs",
+    CONTENTS: "contents",
+    POSTS: "posts",
   },
   site: {
     title: "Ben의 블로그",
--- scripts/build.ts
+++ scripts/build.ts
@@ -1,20 +1,20 @@
 import { mkdir, readFile, readdir, writeFile } from "fs/promises";
 import pug from "pug";
 import config from "../config";
+import matter from "gray-matter";
 
-const { DOCS, VIEWS, SRC } = config.build;
+const { DOCS, VIEWS, SRC, CONTENTS, POSTS } = config.build;
 
 async function renderFile(source: string, dest: string) {
-  const file = await readFile(source);
-  const createIndexTemplate = pug.compile(file.toString(), {
+  const templateFile = await readFile(source);
+  const createIndexTemplate = pug.compile(templateFile.toString(), {
     filename: source,
   });
   const html = createIndexTemplate({ ...config }); // TODO: 동적/정적 값 구분
   writeFile(dest, html);
 }
 
-async function build() {
-  await mkdir(DOCS);
+async function buildHtmlFiles() {
   const files = await readdir(`${SRC}/${VIEWS}`);
   for (const file of files) {
     switch (file) {
@@ -31,6 +31,31 @@ async function build() {
         break;
     }
   }
+}
+
+async function buildMarkdownFiles() {
+  const files = await readdir(`${SRC}/${CONTENTS}`);
+  for (const file of files) {
+    const dirName = file.split(".md")[0];
+    await mkdir(`${DOCS}/${POSTS}/${dirName}`);
+    const postFile = await readFile(`${SRC}/${CONTENTS}/${file}`);
+    // TODO content를 HTML로 변환하기
+    // TODO: data, content를 createPostTemplate의 인자로 전달하기
+    const { data, content } = matter(postFile);
+    const templateFilePath = `${SRC}/${VIEWS}/post.pug`;
+    const templateFile = await readFile(templateFilePath);
+    const createPostTemplate = pug.compile(templateFile.toString(), {
+      filename: templateFilePath,
+    });
+    const html = createPostTemplate({ ...config });
+    writeFile(`${DOCS}/${POSTS}/${dirName}/index.html`, html);
+  }
+}
+
+async function build() {
+  await mkdir(DOCS);
+  await buildHtmlFiles();
+  await buildMarkdownFiles();
 }
 
 build();
