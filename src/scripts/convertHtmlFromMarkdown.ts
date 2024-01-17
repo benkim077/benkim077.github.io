2721ecb4a15dfa32411c5404b1a9ac79cd2c17fb
benkim077 <benkim0077@gmail.com>
2024-01-17 15:47:18 +0900

    이슈 해결 #37
    - 이슈 내용: strikethrough 스타일이 적용되지 않습니다.
    - 해결 방법: Showdown 컨버터 옵션 설정(strikethrough: true)
    - 결과: 이제 `~~`로 감싼 부분이 `<del>` 태그로 변경됩니다.

--- src/scripts/convertHtmlFromMarkdown.ts
+++ src/scripts/convertHtmlFromMarkdown.ts
@@ -11,7 +11,7 @@ const bindings = Object.keys(utilityClassMap).map((key) => ({
         group === "" ? "" : getClassNameFromHtmlClassAttribute(group);
       return `<${key} class="${originClass} ${utilityClassMap[key]}">`;
     }
-
+    // group을 파싱하는 Regex를 사용할 수 있을 것 같은데?
     return group === ""
       ? `<${key} class="${utilityClassMap[key]}">`
       : `<${key} class="${utilityClassMap[key]}" ${group}>`; // 리팩토링 : class, src, alt 를 분리해서 처리할 필요가 있다.
@@ -23,6 +23,7 @@ export default function convertFromMarkdownToHtml(content: string) {
     headerLevelStart: 3,
     extensions: [...bindings],
     noHeaderId: true,
+    strikethrough: true,
   });
   return converter.makeHtml(content);
 }
